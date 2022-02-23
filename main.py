902c2bbe7dacabb647ec52794ac632ef48025a83
dwoods105 <dwoods105@gmail.com>
2022-02-23 11:56:41 -0500

    Add a bunch of server files that I need
    This adds many of the server config files. This will allow changes to the main.py be migrated to the server on push

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "<h1>this is the main flask file"
+    return "<h1>this is the main flask file in the main folder"
