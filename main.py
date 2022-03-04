0b12c4ab025ebb2b461b651e40e3092d43a603eb
EvanTy11 <82114254+EvanTy11@users.noreply.github.com>
2022-03-04 14:07:52 -0500

    Front End to Back End link/editing file structures
    Linked backend to front end with first iteration of flask.
    
    Edited file structures of CSS and HTML files

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return "<h1>this is the main flask file"
+main = Flask(__name__)
+#This is the server connection you can change each attribute to connect to a server of your choice
+main.config['MYSQL_HOST'] = 'localhost'
+main.config['MYSQL_USER'] = 'root'
+main.config['MYSQL_PASSWORD'] = 'dbsucks'
+main.config['MYSQL_DB'] = 'groupproject'
+
+mysql = MySQL(main)
+#Our first route / 
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method =='POST':
+        userDetails = request.form
+        PTID = userDetails['ptID']
+        DOB = userDetails['DoB']
+        SEX = request.form.getlist('radSex')
+        WEIGHT = userDetails['Weight']
+        RPTBY = userDetails['rptBy']
+        FACNAME = userDetails['facName']
+        ADDRESS = userDetails['Address']
+        CITY = userDetails['City']
+        FACILITYSTATE = userDetails['FacilityState']
+        ZIP = userDetails['Zip']
+        PHONE = userDetails['Phone']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Form(ptID, DoB, Sex, Weight, ReportedBy, FacilityName, Address, City, FacilityState, Zip, Phone) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(PTID, DOB, SEX, WEIGHT, RPTBY, FACNAME, ADDRESS, CITY, FACILITYSTATE, ZIP, PHONE))
+        mysql.connection.commit()
+        cur.close()
+        return 'Values have been added'
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+   main.run(debug=True)
